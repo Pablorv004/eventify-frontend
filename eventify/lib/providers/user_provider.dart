3794212f384e7e5282afce79c28605b85eb36caa
RafBel94 <rafael.beltrancaceres@gmail.com>
2024-10-28 17:24:58 +0100

    User Validation

--- eventify/lib/providers/user_provider.dart
+++ eventify/lib/providers/user_provider.dart
@@ -1,16 +1,19 @@
 import 'package:eventify/domain/models/login_response.dart';
 import 'package:eventify/domain/models/user.dart';
+import 'package:eventify/domain/models/validation_response.dart';
 import 'package:eventify/services/login_service.dart';
+import 'package:eventify/services/validation_service.dart';
 import 'package:flutter/material.dart';
 
 class UserProvider extends ChangeNotifier {
   final LoginService loginService;
+  final ValidationService validationService;
   List<User> userList = [];
   User? currentUser;
   String? loginErrorMessage;
+  String? validationErrorMessage;
 
-  UserProvider(this.loginService);
-
+  UserProvider(this.loginService, this.validationService);
 
   Future<void> loginUser(String email, String password) async {
     try {
@@ -29,8 +32,23 @@ class UserProvider extends ChangeNotifier {
     }
   }
 
-  Future<void> fetchAllUsers() async {
+  Future<void> toggleUserValidation(int id, bool validationBoolean) async {
+    try {
+      final validationMethod = validationBoolean ? validationService.activate : validationService.deactivate;
 
+      final validationResponse = await validationMethod(id, currentUser!.rememberToken ?? '');
+
+      final errorMessage = validationBoolean ? 'Validation Failed' : 'Unvalidation Failed';
+
+      validationErrorMessage = validationResponse.success ? null : validationResponse.data['error'] ?? errorMessage;
+    } catch (error) {
+      validationErrorMessage = 'Error: ${error.toString()}';
+    } finally {
+      notifyListeners();
+    }
+  }
+
+  Future<void> fetchAllUsers() async {
     // TODO: IMPLEMENT USER LOADING FROM API
 
     notifyListeners();
