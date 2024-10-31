4580af505730c097d5c2a48af3cad0886563f064
Pablorv004 <pablorv004@gmail.com>
2024-10-31 11:13:22 +0100

    Optimised register screen. Fixed up edit user.

--- eventify/lib/screens/admin/edit_user.dart
+++ eventify/lib/screens/admin/edit_user.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:eventify/domain/models/user.dart';
 import 'package:provider/provider.dart';
