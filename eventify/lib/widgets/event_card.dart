91a1fe66f11cff0143465060e85db29f9055bc60
Pablorv004 <pablorv004@gmail.com>
2024-11-05 10:47:43 +0100

    Updated Event_Card. Updated App_Colors.

--- eventify/lib/widgets/event_card.dart
+++ eventify/lib/widgets/event_card.dart
@@ -1,6 +1,6 @@
+import 'package:eventify/config/app_colors.dart';
 import 'package:eventify/domain/models/event.dart';
 import 'package:flutter/material.dart';
-
 class EventCard extends StatelessWidget {
   final Event event;
   const EventCard({super.key, required this.event});
@@ -28,19 +28,25 @@ class EventCard extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(10.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   event.title,
                   style: const TextStyle(
-                      fontSize: 24, fontWeight: FontWeight.bold),
+                      fontSize: 30, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 5),
-                Text(
-                  event.category,
-                  style: const TextStyle(fontSize: 16),
+                Row(
+                  children: [
+                    const Icon(Icons.calendar_today, size: 25),
+                    const SizedBox(width: 5),
+                    Text(
+                      '${event.startTime}',
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ],
                 ),
               ],
             ),
@@ -50,15 +56,15 @@ class EventCard extends StatelessWidget {
     );
   }
 
-  chooseCardBorderColor(){
-    if(event.category == 'Music'){
-      return Colors.red;
-    } else if(event.category == 'Sport'){
-      return Colors.green;
-    } else if(event.category == 'Technology'){
-      return Colors.orange;
+  chooseCardBorderColor() {
+    if (event.category == 'Music') {
+      return AppColors.musicColor;
+    } else if (event.category == 'Sport') {
+      return AppColors.sportColor;
+    } else if (event.category == 'Technology') {
+      return AppColors.technologyColor;
     } else {
-      return Colors.grey;
+      return AppColors.lightGray;
     }
   }
 }
