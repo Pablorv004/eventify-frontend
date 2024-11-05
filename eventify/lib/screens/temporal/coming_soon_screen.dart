887f1490d031b2d6ff0e6a6ea20a72b40fce962c
RafBel94 <rafael.beltrancaceres@gmail.com>
2024-11-05 12:21:58 +0100

    Filter buttons and User event list

---
+++ eventify/lib/screens/temporal/coming_soon_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ComingSoonScreen extends StatelessWidget {
+  const ComingSoonScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Coming Soon!', style: const TextStyle(fontSize: 30),),
+    );
+  }
+}
---
+++ eventify/lib/screens/user/user_event_screen.dart
@@ -0,0 +1,23 @@
+import 'package:eventify/providers/event_provider.dart';
+import 'package:eventify/providers/user_provider.dart';
+import 'package:eventify/widgets/event_card.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class UserEventScreen extends StatelessWidget {
+  const UserEventScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    EventProvider eventProvider = context.watch<EventProvider>();
+    eventProvider.fetchEvents(context.read<UserProvider>().currentUser!.rememberToken ?? '');
+
+    return ListView.builder(
+      itemCount: eventProvider.eventList.length,
+      itemBuilder: (context, index) {
+        final event = eventProvider.eventList[index];
+        return EventCard(event: event);
+      },
+    );
+  }
+}
