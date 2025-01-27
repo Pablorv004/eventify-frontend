70d412b1027f0bd3caad2141809eb333651608cf
RafBel94 <beltran.caraf23@cadiz.salesianos.edu>
2025-01-27 18:59:47 +0100

    Added notification handling with flutter_local_notifications
    Now notifications are received when app is on foreground, background and even when app is closed

--- eventify/macos/Flutter/GeneratedPluginRegistrant.swift
+++ eventify/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import cloud_firestore
 import firebase_core
 import firebase_messaging
+import flutter_local_notifications
 import location
 import open_file_mac
 import printing
@@ -17,6 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
